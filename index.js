5ac3046076ba6bf510861dc90108bcb4c46f4143
FinzoYFunzo <joaquinandresparada@gmail.com>
2021-05-18 20:25:00 -0400

    2

--- index.js
+++ index.js
@@ -41,7 +41,6 @@ client.on('message', message =>{
 client.on('message', message =>{
   if(message.content.startsWith('wenapo')) {
     message.channel.send('Vieja culia maraca');
-    
   }
 })
 // -----------------------------------------
